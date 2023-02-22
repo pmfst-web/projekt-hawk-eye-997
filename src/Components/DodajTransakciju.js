af025bca175c8bea2b3d14a2db93701004c1a49d
Matijas Salapić <116083161+hawk-eye-997@users.noreply.github.com>
2023-02-22 14:13:26 +0100

    Add files via upload

---
+++ src/Components/DodajTransakciju.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {View, Text} from 'react-native'
+
+function DodajTransakciju() {
+
+  return(
+    <View>
+      <Text>Dodaj Transakciju</Text>
+    </View>
+  );
+};
+
+export default DodajTransakciju;
